f9a44760ec2b56b7621773dc4f38f149cb5fdf88
Zsolt Varga <zs.zsolt.varga@gmail.com>
2020-06-29 18:24:28 +0100

    Inverting repo

---
+++ precache-manifest.1e650f408c8569ae0950d3f83b7663d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec219b9855f579ce3ab7d2dc5a64f5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ce7bae12b77d2f01e6",
+    "url": "/static/css/main.dc03e549.chunk.css"
+  },
+  {
+    "revision": "bf2737ea0f02e035af99",
+    "url": "/static/js/2.f6566b8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6566b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ce7bae12b77d2f01e6",
+    "url": "/static/js/main.1eaf6736.chunk.js"
+  },
+  {
+    "revision": "0201db6fcb28f3b3763c",
+    "url": "/static/js/runtime-main.e6a41e5d.js"
+  }
+]);
